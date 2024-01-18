7c10c462fcc61cd6c029e6502332d71c6c8f7037
Kalu548 <chintuprasad548@gmail.com>
2024-01-18 16:30:13 +0530

    changes

---
+++ cli.py
@@ -0,0 +1,18 @@
+menu = """
++---------------------+------------------------------------------------------+
+| Command             | Function                                             |
++---------------------+------------------------------------------------------+
+| login               | Log in to your account to access your profile.         |
+| register            | Create a new account and register with the system.     |
+| show_all_passwords  | Retrieve a comprehensive list of all saved passwords.  |
+| show_password       | View specific details of a stored password.            |
+| delete_password     | Remove a saved password from your account.             |
+| update_password     | Modify an existing password in your records.           |
+| export              | Export all saved passwords into a CSV file.            |
+| logout              | Safely log out from your account.                      |
++---------------------+------------------------------------------------------+
+"""
+
+
+
+print(menu)
